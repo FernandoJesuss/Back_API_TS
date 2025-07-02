2db1c411af13fb48c34157eca13d8a5cae655c61
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-07-02 20:09:06 -0300

    Feat:Creating transaction summary

--- src/controller/transactions/getTransactionsSummarySchema.controller.ts
+++ src/controller/transactions/getTransactionsSummarySchema.controller.ts
@@ -1,10 +1,18 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
-import { GetTransactionsSummaryQuery } from "../../schemas/transaction.schema";
+import type { GetTransactionsSummaryQuery } from "../../schemas/transaction.schema";
 import utc from "dayjs/plugin/utc";
 import dayjs from "dayjs";
 import prisma from "../../config/prisma";
+import type { CategorySummary } from "../../types/category.types";
+import type { TransactionSummary } from "../../types/transaction.type";
 
 dayjs.extend(utc);
+
+// Adicionar import do TransactionType
+enum TransactionType {
+	expense = "expense",
+	income = "income",
+}
 
 export const getTransactionsSummary = async (
 	request: FastifyRequest<{ Querystring: GetTransactionsSummaryQuery }>,
@@ -19,7 +27,6 @@ export const getTransactionsSummary = async (
 
 	const { month, year } = request.query;
 
-	
 	if (!month || !year) {
 		reply.status(400).send({ error: "Mês e Ano são obrigatórios" });
 		return;
@@ -29,7 +36,6 @@ export const getTransactionsSummary = async (
 	const endDate = dayjs.utc(startDate).endOf("month").toDate();
 
 	try {
-		
 		const transactions = await prisma.transaction.findMany({
 			where: {
 				userId,
@@ -43,9 +49,43 @@ export const getTransactionsSummary = async (
 			},
 		});
 
-		reply.send(transactions);
+		let totalExpenses = 0;
+		let totalIncomes = 0;
+		const groupedExpenses = new Map<string, CategorySummary>();
+
+		for (const transaction of transactions) {
+			if (transaction.type === TransactionType.expense) {
+				const existing = groupedExpenses.get(transaction.categoryId) ?? {
+					categoryId: transaction.categoryId,
+					categoryName: transaction.Category.name,
+					categoryColor: transaction.Category.color,
+					amount: 0,
+					percentage: 0,
+				};
+
+				existing.amount += transaction.amount;
+				groupedExpenses.set(transaction.categoryId, existing);
+
+				totalExpenses += transaction.amount;
+			} else {
+				totalIncomes += transaction.amount;
+			}
+		}
+
+		const summary: TransactionSummary = {
+			totalExpenses,
+			totalIncomes,
+			balance: totalIncomes - totalExpenses,
+			expensesByCategory: Array.from(groupedExpenses.values()).map(
+				(entry) => ({
+					...entry,
+					percentage: Number.parseFloat(((entry.amount / totalExpenses) * 100).toFixed(2)),
+				})
+			).sort((a, b) => b.amount - a.amount),
+		};
+
+		reply.send(summary);
 	} catch (err) {
-		
 		request.log.error("Erro ao buscar transações", err);
 		reply.status(500).send({ error: "Erro do servidor" });
 	}
